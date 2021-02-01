fa017a4888df3b654452ea335e4c5345c6507431
Dima <topface26007@gmail.com>
2021-02-01 20:23:19 +0200

    toggle

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,4 +6,27 @@ import {Component} from '@angular/core';
   styleUrls: ['card.component.scss']
 })
 
-export class CardComponent {}
+export class CardComponent implements OnInit{
+  title = 'My Card Title';
+  text = 'My sample text';
+  textColor: string;
+  // tslint:disable-next-line:typedef
+  ngOnInit() {}
+
+  // tslint:disable-next-line:typedef
+  changeTitle() {
+    this.title = 'Title has been changed!';
+  }
+
+  // tslint:disable-next-line:typedef
+  inputHandler(value) {
+    // const value = event.target.value;
+    this.title = value;
+  }
+
+  // tslint:disable-next-line:typedef
+  changeHandler() {
+
+  }
+}
+
